fa38b150c41a943489bc5b2292efc0dcce22881c
Eespaka <171569728+Eespaka@users.noreply.github.com>
2024-06-04 23:22:34 +0400

    Update 4.cs

--- task 4 /4.cs
+++ task 4 /4.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Linq;
 
+class Program
+{
+    static int MinStepsToUnify(int[]nums)
+{
+int target = nums.Min();
+int steps = 0;
+foreach (int num in nums)
+{
+    steps += Math.Abs(num - target);
+}
+return steps;
+}
+
+static void Main()
+{
+    int[] nums = new int[]{1,2,3};
+    Console.WriteLine(MinStepsToUnify(nums));
+} 
+}
